5839bb314e7c9ca0c8aa468ee5b7684f09cc771b
Kat <bolivarkat@yahoo.com>
2021-09-09 19:37:49 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function superbowlWin(array) {
+    let winningYear = array.find(game => game.result === "W")
+    if (winningYear){
+        return winningYear.year
+    } else {
+        return undefined
+    }
+}
